e7c6a90ec11f74524692e47aec95fc7c6a78b6eb
ruanvalente <ruanopensuselinux@gmail.com>
2025-12-30 10:54:51 -0300

    feat: add line and bar chart in dashboard page

--- src/app/presentation/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/presentation/pages/dashboard-page/dashboard-page.component.ts
@@ -1,7 +1,10 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { LineChartComponent } from '@/shared/ui/chart/line/line-chart.component';
+import { BarChartComponent } from '@/shared/ui/chart/bar/bar-chart.component';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
+  imports: [LineChartComponent, BarChartComponent],
 })
 export class DashboardPageComponent {}
